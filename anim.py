69cad4db01d18f9b996b9dae684bc0eda14aba04
Tom Cariello <tom.cariello@oddsium.com>
2022-06-28 17:03:47 -0400

    Simple static plot being generated with dummy data

--- anim.py
+++ anim.py
@@ -1,16 +1,20 @@
-from matplotlib import animation
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-import seaborn as sns
-# %matplotlib qt
 
-fig = plt.figure(figsize=(8,6))
-axes = fig.add_subplot(1,1,1)
-axes.set_ylim(0,150)
-plt.style.use("seaborn")
+# Create a new figure
+fig = plt.figure()
 
-lst1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-lst2 = [0,5,10,15,20,25,30,35,40,50,60,70,80,90,100]
+# Add axes to the figure
+ax = fig.add_axes([0,0,1,1])
 
-print(len(lst1), len(lst2))
+# Indicate the labels
+langs = ['C', 'C++', 'Java', 'Python', 'PHP']
 
+# Student values list 
+students = [23,17,35,29,12]
+
+# Specify the chart type (barh --> horizontal bar chart)
+# Also specify which values to show on each axis
+ax.barh(langs,students)
+
+# Show the bar chart
+plt.show()

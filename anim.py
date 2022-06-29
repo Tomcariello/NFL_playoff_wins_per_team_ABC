491855726543a7f83929b7d7789cfa206aecbca6
Tom Cariello <tom.cariello@oddsium.com>
2022-06-29 09:51:31 -0400

    Properly centered the figure inside the window. Axis labels are displaying.

--- anim.py
+++ anim.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 # Create a new figure
 fig = plt.figure()
 
-# Add axes to the figure
-ax = fig.add_axes([0,0,1,1])
+# Add axes to the figure [x0, y0, width, height]
+# x0 = where to start the x-axis, relative to the left side of the window
+# y0 = where to start the y-axis, relative to the bottom of the window
+# width = the width this axes should occupy as a percentage of the window
+# height = the height this axes should occupy as a percentage of the window
+ax = fig.add_axes([.25,.25,.5,.5])
 
 # Indicate the labels
 languages = ['JavaSCript', 'C++', 'Java', 'Python', 'PHP']
@@ -15,6 +19,8 @@ people = [11,58,27,36,21]
 # Specify the chart type (barh --> horizontal bar chart)
 # Also specify which values to show on each axis
 ax.barh(languages, people)
+
+# plt.axis("image")
 
 # Show the bar chart
 plt.show()
